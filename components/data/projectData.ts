9fb6159f35c3c42152e9aa48b9cd17eaf6b671c1
kabornblack <shaibu.kabiru@email.com>
2025-04-28 06:29:09 +0300

    data

---
+++ components/data/projectData.ts
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    title: "Global Update",
+    description: "Stay upto date with Global information",
+    githubUrl: "https://github.com/kabornblack/hello-world",
+    liveUrl: "https://global-update-nine.vercel.app/",
+    tags: ["React", "NextJs", "TypeScript"],
+    image: "/global.png",
+  },
+  {
+    title: "C-U school",
+    description: "Educational website with language translation.",
+    githubUrl: "https://github.com/kabornblack/C-U-Language-School",
+    liveUrl: "https://www.cuilschool.ee/",
+    tags: ["React", "NextJs", "TypeScript"],
+    image: "/cuschool.png",
+  },
+  {
+    title: "Swapify",
+    description: "Barter and donation platform either exchange or donate.",
+    githubUrl: "https://github.com/swapify-ou",
+    liveUrl: "https://www.swapify.ee",
+    tags: ["React", "JavaScript", "NextJs", "Dart"],
+    image: "/swapify.png",
+  },
+  {
+    title: "Upto-date",
+    description: "Users can share a fact and other can like or dislike",
+    githubUrl: "https://github.com/kabornblack/Upto-date",
+    liveUrl: "https://uptodate-kabbi.netlify.app/",
+    tags: ["HTML", "CSS", "React", "Superbase"],
+    image: "/uptodate.png",
+  },
+  {
+    title: "Disney-Clone",
+    description: "A clone of the Disney website for educational purpose",
+    githubUrl: "https://github.com/kabornblack/Disney-clone",
+    liveUrl: "https://disney-clone-omega-henna.vercel.app/",
+    tags: ["React", "NextJs", "TypeScript"],
+    image: "/disneyclone.png",
+  },
+];
